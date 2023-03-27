50a1960070343374782c4491c48cec37b1d71146
DymitrDworakowski <116100679+DymitrDworakowski@users.noreply.github.com>
2023-03-27 12:44:57 +0200

    1 i 7

--- js/task-01.js
+++ js/task-01.js
@@ -1,13 +1,22 @@
-const listItem = document.querySelector('#categories');
-const items = listItem.querySelectorAll('.item')
+// const listItem = document.querySelector('#categories');
+// const items = listItem.querySelectorAll('.item')
 
 
-console.log(`Number of categories: ${items.length}`);
+// console.log(`Number of categories: ${items.length}`);
 
-items.forEach((category) => {
-const categoryTitle = category.querySelector("h2").textContent;
-const categoryElements = category.querySelectorAll("li").length;
+// items.forEach((category) => {
+// const categoryTitle = category.querySelector("h2").textContent;
+// const categoryElements = category.querySelectorAll("li").length;
 
-console.dir(`Category: ${categoryTitle}`);
-console.dir(`Elements: ${categoryElements}`);
+// console.dir(`Category: ${categoryTitle}`);
+// console.dir(`Elements: ${categoryElements}`);
+// });
+
+const cat = document.getElementById('categories').children.length;
+console.log(`Number of categories: ${cat}`);
+const array = document.querySelectorAll('h2');
+
+array.forEach((title) => {
+  console.log('Category:', title.textContent);
+  console.log('Elements:', title.nextElementSibling.children.length);
 });
